4648cf13a3a73273dee5bc2f77ef66e0434ec4ec
lenakchen <klingx@gmail.com>
2016-07-27 16:58:41 -0700

    Create pi_series_generator.py

---
+++ pi_series_generator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# The infinite series: 1 - 1/3 + 1/5 - 1/7 ... converges to π/4 
+# write a generator function which progressively makes better and better approximations of π
+
